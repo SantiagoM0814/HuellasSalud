ce821f9f613f4619d9c01e13ec0326b6bec4bce5
Santiago Muñoz Aponte <santiagom0814@hotmail.com>
2025-10-03 15:32:48 -0500

    Se cambia puerto de envio correo

--- huellas-salud-back/src/main/java/org/huellas/salud/services/MailService.java
+++ huellas-salud-back/src/main/java/org/huellas/salud/services/MailService.java
@@ -126,11 +126,10 @@ public class MailService {
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.starttls.required", "true");
-        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587"); // TLS
+        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
 
         Session session = Session.getInstance(props, new Authenticator() {
             @Override
